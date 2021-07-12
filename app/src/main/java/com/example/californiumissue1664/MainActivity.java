6595ea507ec886354e5084c626ba48aa00d85b40
thomas <thomasjsvos@gmail.com>
2021-07-12 15:24:15 +0200

    Issue with Californium 3.0.0-SNAPSHOT

--- app/src/main/java/com/example/californiumissue1664/MainActivity.java
+++ app/src/main/java/com/example/californiumissue1664/MainActivity.java
@@ -13,9 +13,14 @@ import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.coap.CoAP;
 import org.eclipse.californium.core.coap.Request;
+import org.eclipse.californium.elements.util.NetworkInterfacesUtil;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.URL;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
@@ -26,12 +31,14 @@ public class MainActivity extends AppCompatActivity {
 
     private static final Executor executor = Executors.newSingleThreadExecutor();
 
+    ConnectivityManager connectivityManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ConnectivityManager connectivityManager = getSystemService(ConnectivityManager.class);
+        connectivityManager = getSystemService(ConnectivityManager.class);
 
         ConnectivityManager.NetworkCallback callback = new ConnectivityManager.NetworkCallback() {
             @Override
@@ -84,23 +91,72 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void executeCaliforniumRequest() {
-//        String uri = "coap://californium.eclipseprojects.io:5683/";
-        String uri = "coap://35.185.40.182:5683/"; // same as above url
 
         executor.execute(() -> {
+            // This HTTP request should print an exception.
+            doHttpRequest();
+            // This Californium request should print null.
+            doCaliforniumRequest();
+
+            // Clear binding so network requests are allowed again.
+            connectivityManager.bindProcessToNetwork(null);
+
+            // Confirm that the network interfaces are available again, they are in my case.
+            printNetworkInterfaces();
+
+            NetworkInterfacesUtil.clear();
+
+            // Add a small delay otherwise HTTP network request fails. (I do not know why this is needed)
             try {
-                CoapClient client = new CoapClient(uri);
-                Request request = new Request(CoAP.Code.GET);
-                request.setURI(uri);
-                CoapResponse response = client.advanced(request);
-                if (response == null) {
-                    System.out.println("Coap response: " + null);
-                } else {
-                    System.out.println("Coap response: " + response.getResponseText());
-                }
-            } catch (Exception exception) {
-                exception.printStackTrace();
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
+
+            // HTTP request should work again.
+            doHttpRequest();
+
+            // I would expect this Californium request to work again, but it does not for me.
+            // It still prints null.
+            doCaliforniumRequest();
         });
+    }
+
+    private void doCaliforniumRequest() {
+        try {
+//        String uri = "coap://californium.eclipseprojects.io:5683/";
+            String uri = "coap://35.185.40.182:5683/"; // same as above url
+            CoapClient client = new CoapClient();
+            Request request = new Request(CoAP.Code.GET);
+            request.setURI(uri);
+            CoapResponse response = client.advanced(request);
+            if (response == null) {
+                System.out.println("Coap response: " + null);
+            } else {
+                System.out.println("Coap response size: " + response.getResponseText().length());
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void doHttpRequest() {
+        try {
+            URL url = new URL("http://example.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.connect();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String content = "", line;
+            while ((line = rd.readLine()) != null) {
+                content += line + "\n";
+            }
+            System.out.println("doHttpRequest result size: " + content.length());
+        } catch (Exception exception) {
+            System.out.println("doHttpRequest result: " + exception);
+        }
     }
 }
